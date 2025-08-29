e855578cf3ac28f5a46431c9845d3d2ab2e754e0
Alex Morehead <alex.morehead@gmail.com>
2025-08-29 00:31:22 +0000

    Log validation metrics more often during train sweeping

--- scripts/perlmutter/regular/sweep_gpu.sh
+++ scripts/perlmutter/regular/sweep_gpu.sh
@@ -34,7 +34,7 @@ mkdir -p "$TORCH_HOME"
 mkdir -p "$HF_HOME"
 
 # Define run details
-DEFAULT_SWEEP_ID="tksufrfx"                   # NOTE: Generate a unique ID for each run by running `wandb sweep configs/sweep/{train,eval}_sweep_{joint,qm9,mp20}.yaml`
+DEFAULT_SWEEP_ID="lu4eoirj"                   # NOTE: Generate a unique ID for each run by running `wandb sweep configs/sweep/{train,eval}_sweep_{joint,qm9,mp20}.yaml`
 SWEEP_ID=${1:-$DEFAULT_SWEEP_ID}              # First argument or default ID if not provided
 
 # Inform user of job details
