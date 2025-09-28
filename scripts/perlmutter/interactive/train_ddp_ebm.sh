da80a012d403f31d3ceb987c2dec20b847e61999
Alex Morehead <alex.morehead@gmail.com>
2025-09-27 19:50:05 -0700

    Set up new interactive job

--- scripts/perlmutter/interactive/train_ddp_ebm.sh
+++ scripts/perlmutter/interactive/train_ddp_ebm.sh
@@ -34,8 +34,8 @@ NHEAD=12  # 6, 12, 16
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`, `qm9_only`, `mp20_only`, `qmof150_only`, `omol25_only`, `geom_only`)
-DEFAULT_RUN_ID="hv0mirui"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
-DEFAULT_RUN_DATE="2025-09-25_19-00-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
+DEFAULT_RUN_ID="iv0mirui"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_DATE="2025-09-27_20-00-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
 RUN_NAME="MFT-B__${DATASET}_subset_s1_debugging"       # Name of the model type and dataset configuration
