27072c1524a75444b17cace49c033cc38bb69a04
Alex Morehead <alex.morehead@gmail.com>
2025-10-18 20:52:57 +0000

    Use sweep to try out logit-normal timestep sampling across all model architectures

--- scripts/perlmutter/regular/sweep_gpu.sh
+++ scripts/perlmutter/regular/sweep_gpu.sh
@@ -34,7 +34,7 @@ mkdir -p "$TORCH_HOME"
 mkdir -p "$HF_HOME"
 
 # Define run details
-DEFAULT_SWEEP_ID="1k2b6il4"                   # NOTE: Generate a unique ID for each run by running `wandb sweep configs/sweep/{train,eval}_sweep_{joint,qm9,mp20,qmof150,omol25,geom}.yaml`
+DEFAULT_SWEEP_ID="gd0h50vo"                   # NOTE: Generate a unique ID for each run by running `wandb sweep configs/sweep/{train,eval}_sweep_{joint,qm9,mp20,qmof150,omol25,geom}.yaml`
 SWEEP_ID=${1:-$DEFAULT_SWEEP_ID}              # First argument or default ID if not provided
 
 # Inform user of job details
