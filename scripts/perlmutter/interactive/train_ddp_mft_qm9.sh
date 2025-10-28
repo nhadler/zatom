03b06f9530c53f6131c0732d81d6627577f5bbe0
Alex Morehead <alex.morehead@gmail.com>
2025-10-28 21:56:22 +0000

    Set up new joint training run after seeing MP20 run plateau in terms of val performance

--- scripts/perlmutter/interactive/train_ddp_mft_qm9.sh
+++ scripts/perlmutter/interactive/train_ddp_mft_qm9.sh
@@ -9,7 +9,7 @@
 #        --gpus-per-node=2 \
 #        --ntasks-per-node=2 \
 #        --time=04:00:00 \
-#        --job-name=mft-80M
+#        --job-name=mft-80M-qm9
 
 # Determine location of the project's directory
 # PROJECT_ID="dasrepo"
@@ -28,8 +28,8 @@ mkdir -p "$HF_HOME"
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`,)
-DEFAULT_RUN_ID="fq48mptx"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
-DEFAULT_RUN_DATE="2025-10-28_15-00-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
+DEFAULT_RUN_ID="brfaul2g"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_DATE="2025-10-27_18-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 DEFAULT_ARCHITECTURE="mft_80M"            # NOTE: Set the model architecture to be used, must be one of (`{mft,met,mfp}_80M`, `{mft,met,mfp}_180M`, `{mft,met,mfp}_500M`)
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
@@ -37,8 +37,8 @@ RUN_ID=${2:-$DEFAULT_RUN_ID}              # Second argument or default ID if not
 RUN_DATE=${3:-$DEFAULT_RUN_DATE}          # Third argument or default date if not provided
 ARCHITECTURE=${4:-$DEFAULT_ARCHITECTURE}  # Fourth argument or default architecture if not provided
 
-TASK_NAME="train_fm"                                  # Name of the task to perform
-RUN_NAME="train_arch-${ARCHITECTURE}_${DATASET}"      # Name of the model type and dataset configuration
+TASK_NAME="train_fm"                           # Name of the task to perform
+RUN_NAME="train_arch-${ARCHITECTURE}_QM9"      # Name of the model type and dataset configuration
 
 CKPT_PATH="logs/$TASK_NAME/runs/${RUN_NAME}_${RUN_DATE}/checkpoints/" # Path at which to find model checkpoints
 mkdir -p "$CKPT_PATH"
@@ -69,8 +69,10 @@ bash -c "
     unset NCCL_CROSS_NIC \
     && HYDRA_FULL_ERROR=1 WANDB_RESUME=allow WANDB_RUN_ID=$RUN_ID TORCH_HOME=$TORCH_HOME HF_HOME=$HF_HOME \
     srun --kill-on-bad-exit=1 shifter python zatom/$TASK_NAME.py \
+    callbacks.model_checkpoint.monitor=val_qm9/valid_rate \
     ckpt_path=$CKPT_PATH \
     data=$DATASET \
+    data.datamodule.datasets.mp20.proportion=0.0 \
     date=$RUN_DATE \
     experiment=train \
     model/architecture=$ARCHITECTURE \
