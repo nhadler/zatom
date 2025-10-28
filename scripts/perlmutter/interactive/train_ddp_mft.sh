64fcbcfb0052e630ee9d5ba95deaa40b44557f5c
Alex Morehead <alex.morehead@gmail.com>
2025-10-27 17:31:14 -0700

    Reset interactive QM9 and MP20 runs, after increasing default LR from 1e-4 to 1e-3 and changing default AdamW betas from [0.9, 0.999] to [0.9, 0.995] following Tabasco

--- scripts/perlmutter/interactive/train_ddp_mft.sh
+++ scripts/perlmutter/interactive/train_ddp_mft.sh
@@ -28,8 +28,8 @@ mkdir -p "$HF_HOME"
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`,)
-DEFAULT_RUN_ID="wnv94pae"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
-DEFAULT_RUN_DATE="2025-10-24_12-00-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
+DEFAULT_RUN_ID="5pdd7tiu"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_DATE="2025-10-27_17-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 DEFAULT_ARCHITECTURE="mft_80M"            # NOTE: Set the model architecture to be used, must be one of (`{mft,met,mfp}_80M`, `{mft,met,mfp}_180M`, `{mft,met,mfp}_500M`)
 
 DATASET=${1:-$DEFAULT_DATASET}            # First argument or default dataset if not provided
