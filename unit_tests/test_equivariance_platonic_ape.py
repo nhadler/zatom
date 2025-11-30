c21f67f65a32c3bf0a42c538d6559df3f28df009
Maurice Weiler <m.weiler.ml@gmail.com>
2025-11-29 19:07:55 -0500

    Platonic absolute position embeddings + equivariance unit tests

---
+++ unit_tests/test_equivariance_platonic_ape.py
@@ -0,0 +1,88 @@
+import unittest
+from functools import partial
+from typing import Literal
+
+import torch
+from platonic_transformers.models.platoformer.groups import PLATONIC_GROUPS
+
+from zatom.models.architectures.transformer.positional_encoder import (
+    PlatonicLinearAPE,
+    PlatonicSinusoidAPE,
+)
+
+
+class TestEquivariancePlatonicAPE(unittest.TestCase):
+    """Equivariance unit tests for Platonic group equivariant absolute position embeddings.
+
+    These modules are only equivariant wrt rotations and reflections in their Platonic group <
+    O(N). They break translation equivariance.
+    """
+
+    def test_equivariance_platonic_sinusoidal_ape(self):
+        """Running PlatonicSinusoidAPE equivariance tests for all Platonic groups."""
+        for solid_name in PLATONIC_GROUPS:
+            self._test_equivariance_platonic_ape(solid_name, PlatonicAPEClass=PlatonicSinusoidAPE)
+
+    def test_equivariance_platonic_linear_ape(self):
+        """Running PlatonicLinearAPE equivariance tests for all Platonic groups."""
+        for solid_name in PLATONIC_GROUPS:
+            self._test_equivariance_platonic_ape(solid_name, PlatonicAPEClass=PlatonicLinearAPE)
+
+    def _test_equivariance_platonic_ape(
+        self,
+        solid_name: str,
+        PlatonicAPEClass: Literal[PlatonicSinusoidAPE, PlatonicLinearAPE],
+    ) -> None:
+        """Shared equivariance test for the given Platonic APE embedding class and symmetry group.
+
+        Looping over all group elements.
+        """
+
+        group = PLATONIC_GROUPS[solid_name]
+        num_G = group.G
+        embed_dim = 16 * num_G  # divisible by num_G
+        B, N = 2, 4
+        class_name = PlatonicAPEClass.__name__
+        print(f"testing {class_name}, G={solid_name}               ", end="\r")
+
+        torch.manual_seed(42)
+        pos = torch.randn(B, N, group.dim)
+
+        if PlatonicAPEClass is PlatonicSinusoidAPE:
+            PlatonicAPEClass = partial(PlatonicAPEClass, freq_sigma=4.2)
+
+        pos_embedder = PlatonicAPEClass(
+            embed_dim=embed_dim,
+            solid_name=solid_name,
+            spatial_dims=group.dim,
+        )
+
+        all_tests_passed = True
+        embedding_orig = pos_embedder(pos)
+
+        for g in range(num_G):
+            g_indices = group.cayley_table[g, :]
+            g_element = group.elements[g].to(pos.dtype)
+
+            # Compute APE(g.pos)
+            pos_transformed = torch.einsum(
+                "ji,...j ->...i", g_element, pos
+            )  # NOTE: acting as transposed element ???
+            embedding_ape_g = pos_embedder(pos_transformed)
+
+            # Compute g.APE(pos)
+            embedding_g_ape = embedding_orig.view(B, N, num_G, embed_dim // num_G)
+            embedding_g_ape = embedding_g_ape[:, :, g_indices, :]
+            embedding_g_ape = embedding_g_ape.reshape(B, N, embed_dim)
+
+            self.assertTrue(
+                torch.allclose(embedding_ape_g, embedding_g_ape, atol=1e-5),
+                "\nEquivariance test failed for:\n"
+                + f"  {class_name}\n"
+                + f"  G = {solid_name}\n"
+                + f"  max difference: {torch.max(torch.abs(embedding_g_ape - embedding_ape_g))}",
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
