cf2a279c03995e12463d5ae472c36e15626b298c
Alex Morehead <alex.morehead@gmail.com>
2025-11-30 05:32:38 +0000

    Increase time limit for finetuning

--- scripts/perlmutter/interactive/finetune_ddp_tft_joint.sh
+++ scripts/perlmutter/interactive/finetune_ddp_tft_joint.sh
@@ -85,7 +85,7 @@ bash -c "
     task_name=$TASK_NAME \
     trainer.num_nodes=$SLURM_JOB_NUM_NODES \
     trainer.devices=$SLURM_NTASKS_PER_NODE \
-    trainer.max_time='06:00:00:00'
+    trainer.max_time='08:00:00:00'
 "
 
 # Inform user of run completion
