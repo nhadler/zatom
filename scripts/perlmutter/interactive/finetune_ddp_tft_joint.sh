108f5ebda438c77be6e7fd09d06f9866779de380
Alex Morehead <alex.morehead@gmail.com>
2025-12-08 23:30:37 +0000

    Increase OMol25 finetuning batch size from 32 to 64 now that only relevant weights are being finetuned

--- scripts/perlmutter/interactive/finetune_ddp_tft_joint.sh
+++ scripts/perlmutter/interactive/finetune_ddp_tft_joint.sh
@@ -28,7 +28,7 @@ mkdir -p "$HF_HOME"
 
 # Define run details
 DEFAULT_DATASET="joint"                   # NOTE: Set the dataset to be used, must be one of (`joint`,)
-DEFAULT_RUN_ID="f87lowlu"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
+DEFAULT_RUN_ID="yrfz1iew"                 # NOTE: Generate a unique ID for each run using `python scripts/generate_id.py`
 DEFAULT_RUN_DATE="2025-12-08_15-30-00"    # NOTE: Set this to the initial date and time of the run for unique identification (e.g., ${now:%Y-%m-%d}_${now:%H-%M-%S})
 DEFAULT_MODEL="zatom"                     # NOTE: Set the model to be used, must be one of (`zatom`,)
 DEFAULT_EXPERIMENT="finetune"             # NOTE: Set the experiment name to be used, must be one of (`train`, `finetune`, `eval`, `overfit`)
@@ -77,9 +77,9 @@ bash -c "
     pretrained_ckpt_path=$PRETRAINED_CKPT_PATH \
     ckpt_path=$CKPT_PATH \
     data=$DATASET \
-    data.datamodule.batch_size.train=32 \
-    data.datamodule.batch_size.val=32 \
-    data.datamodule.batch_size.test=32 \
+    data.datamodule.batch_size.train=64 \
+    data.datamodule.batch_size.val=64 \
+    data.datamodule.batch_size.test=64 \
     data.datamodule.datasets.qm9.proportion=0.0 \
     data.datamodule.datasets.qm9.global_property=null \
     data.datamodule.datasets.omol25.proportion=1.0 \
